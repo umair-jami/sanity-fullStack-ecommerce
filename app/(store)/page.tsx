98140b8b1c204519ed4b1ddf7ad7562d5e7899c7
umair-jami <lalaumair92@gmail.com>
2025-01-01 15:01:34 +0500

    updated code

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -2,23 +2,18 @@ import BlackFridayBanner from "@/components/BlackFridayBanner";
 import ProductView from "@/components/ProductView";
 import { getAllCategories } from "@/sanity/lib/products/getallCategories";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
-import { Poppins } from "next/font/google";
 
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
 export const dynamic="force-static"
 export const revalidate=60
 export default async function Home() {
   const products = (await getAllProducts()) ?? [];
+  console.log("Sanity token is available:",process.env.SANITY_API_TOKEN);
   const categories = (await getAllCategories()) ?? [];
   console.log(
     crypto.randomUUID().slice(0,5)+ `>>> Rerender the home page cache with ${products.length} products and ${categories.length} categories`
   )
   return (
-    <div className={poppins.className}>
+    <div>
       <BlackFridayBanner />
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <ProductView products={products} categories={categories} />
