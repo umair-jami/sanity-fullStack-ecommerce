a2627703f7a951b90800e3cc3c3bfce351d0aeae
umair-jami <lalaumair92@gmail.com>
2025-01-01 11:24:34 +0500

    update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,15 @@ import Form from "next/form";
 import { PackageIcon, TrolleyIcon } from "@sanity/icons";
 import { SignedIn } from "@clerk/clerk-react";
 import userBasketStore from "@/store/store";
+import { Poppins } from "next/font/google";
+import logo from "../public/Orange and Gray Tag Cart Virtual Shop Logo (1).png"
+import Image from "next/image";
+import { log } from "console";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function Header() {
   const { user } = useUser();
@@ -21,14 +30,20 @@ export default function Header() {
     }
   };
   return (
-    <header className="flex flex-wrap justify-between items-center px-4 py-2">
+    <header className={`${poppins.className} flex flex-wrap justify-between items-center px-4 py-2`}>
       {/* Top row */}
       <div className="flex w-full justify-between flex-wrap items-center">
         <Link
           href="/"
           className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
         >
-          Shoper
+          <Image
+          src={logo}
+          alt="logo"
+          width={100}
+          height={100}
+          className="rounded-md"
+          />
         </Link>
         <Form
           action="/search"
@@ -44,7 +59,7 @@ export default function Header() {
         <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 md:flex-none">
           <Link
             href="/basket"
-            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md font-bold"
+            className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 border hover:scale-105 transition-all duration-200 border-b-black py-2 px-4 rounded-md font-bold"
           >
             <TrolleyIcon className="w-6 h-6" />
             {/* Span Item count once glbal state is implemented */}
@@ -57,7 +72,7 @@ export default function Header() {
             <SignedIn>
               <Link
                 href="/orders"
-                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md font-bold"
+                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2  border border-b-black py-2 px-4 rounded-md font-bold hover:scale-105 transition-all duration-200"
               >
                 <PackageIcon className="w-6 h-6" />
                 {/* Span Item count once glbal state is implemented */}
--- components/ProductThumb.tsx
+++ components/ProductThumb.tsx
@@ -21,12 +21,12 @@ function ProductThumb({ product }: { product: Product }) {
       href={`/product/${product.slug?.current}`}
       className={`group flex flex-col h-full w-full bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-md transition-all duration-200 overflow-hidden ${isOutOfStock ? "opacity-50" : ""}`}
     >
-      <div className="relative aspect-square w-full overflow-hidden h-full">
+      <div className="relative aspect-square w-[80%] mx-auto overflow-hidden h-full">
         {product.image && (
           <Image
             src={imageUrl(product.image).url()}
             alt={product.name || "product image"}
-            className="object-contain transition-transform duration-300 group-hover:scale-105"
+            className="object-cover transition-transform duration-300 group-hover:scale-105"
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
